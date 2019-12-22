fe73ff594a19a81dd4901ddd63148986e356715e
shaovoon <shaovoon@yahoo.com>
2019-12-22 14:31:47 +0800

    Put comments on pch.h

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Lee algorithm maze solver in Visual C++ MFC and Direct2D
 ![Image of Simulator](https://github.com/shaovoon/lee_mazesolver/blob/master/screenshot.png)
 
 [Lee Algorithm on Wikipedia](https://en.wikipedia.org/wiki/Lee_algorithm)
+[Direct2D Fundamentals on Pluralsight](https://app.pluralsight.com/library/courses/direct2d-fundamentals/table-of-contents)
--- TestMazeSolver/pch.h
+++ TestMazeSolver/pch.h
@@ -9,17 +9,19 @@
 
 // add headers that you want to pre-compile here
 #include "framework.h"
-#include <wrl.h>
-#include <d2d1.h>
-#include <dwrite.h>
-#include <stdexcept>
+#include <wrl.h> // Windows Runtime Library for its ComPtr
+#include <d2d1.h> // Direct2D v1 header
+#include <dwrite.h> // DirectWrite header
+#include <stdexcept> // C++11 exception class
 
 #include "ComException.h"
-#include "FactorySingleton.h"
+#include "FactorySingleton.h" // Singleton to get Direct2D factories
 
+// Direct2D and DirectWrite import libraries
 #pragma comment(lib, "d2d1")
 #pragma comment(lib, "dwrite")
 
+// Macro to test HRESULT and throw ComException for failed HRESULT
 #define HR(expression) TestResult(#expression, (expression));
 
 
